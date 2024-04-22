5064db742ddbd3870b4244775c3b30e8aab9b3bb
GyMano <markmano@stud.uni-obuda.hu>
2024-04-22 12:04:19 +0200

    /home/index updated

--- src/Modules/OL2.Core/Controllers/HomeController.cs
+++ src/Modules/OL2.Core/Controllers/HomeController.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OrchardCore.DisplayManagement.Notify;
 
 namespace OL2.Core.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer T;
+        private readonly INotifier _notifier;
+        private readonly IHtmlLocalizer H;
+        private readonly ILogger _logger;
+
+        public HomeController(IStringLocalizer<HomeController> stringLocalizer, INotifier notifier, IHtmlLocalizer<HomeController> htmlLocalizer, ILogger<HomeController> logger)
+        {
+            _notifier = notifier;
+            _logger = logger;
+            H = htmlLocalizer;
+            T = stringLocalizer;
+        }
+
         public ActionResult Index()
         {
+            ViewData["Message"] = T["Hello world from controller!"];
+            _notifier.SuccessAsync(H["Localized HTML"]);
+            _logger.LogWarning("WarningMessage");
             return View();
         }
     }
